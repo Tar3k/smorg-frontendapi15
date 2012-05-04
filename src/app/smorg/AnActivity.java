17a21af0c2e60a891a6fcf15876786cc04745f05
Tarek Assem <tarek.assemgamal@gmail.com>
2012-05-04 22:28:14 +0200

    first commit

---
+++ src/app/smorg/AnActivity.java
@@ -0,0 +1,13 @@
+package app.smorg;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AnActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
